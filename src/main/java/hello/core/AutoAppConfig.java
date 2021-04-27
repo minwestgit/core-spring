388f2f6f6a8e3098e41796a45a425d69092f8f1b
minwestgit <minwest61@gmail.com>
2021-04-27 21:27:47 +0900

    컴포넌트 스캔 추가 & 필터

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.springframework.context.annotation.ComponentScan.*;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core", // 탐색할 패키지의 시작 위치 지정
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) // TestConfig 같은 것들도 빈으로 등록되기 때문에 필터 처리한 것.
+public class AutoAppConfig {
+
+}
