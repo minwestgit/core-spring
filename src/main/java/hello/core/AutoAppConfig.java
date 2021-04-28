be220152af2f4a8f03b71db18cdfebc1275af3e1
minwestgit <minwest61@gmail.com>
2021-04-28 22:29:16 +0900

    수동빈 등록 vs 자동빈 등록 => 수동빈이 우선권을 가짐

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) // TestConfig 같은 것들도 빈으로 등록되기 때문에 필터 처리한 것.
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
