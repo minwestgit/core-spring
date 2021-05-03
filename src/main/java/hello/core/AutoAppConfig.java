7082e041bf4541e862def3158b82d32aa792b3a7
minwestgit <minwest61@gmail.com>
2021-05-03 22:59:56 +0900

    Autowired 옵션처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -14,9 +14,10 @@ import static org.springframework.context.annotation.ComponentScan.*;
         basePackages = "hello.core", // 탐색할 패키지의 시작 위치 지정
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) // TestConfig 같은 것들도 빈으로 등록되기 때문에 필터 처리한 것.
 public class AutoAppConfig {
-
+    /*
     @Bean(name = "memoryMemberRepository")
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    */
 }
