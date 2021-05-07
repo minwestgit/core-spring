51b821e2781d052b257014928f104babf766afa9
minwestgit <minwest61@gmail.com>
2021-05-07 22:18:59 +0900

    조회 대상 빈이 2개 이상일 때. - @Autowired 필드 명 매칭 - @Qualifier -> @Qualifier끼리 매칭 -> 빈 이름 매칭 - @Primary 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,35 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor // final이 붙은 필드를 모아서 생성자를 자동으로 만들어줌.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    /*
+    @Autowired
+    private DiscountPolicy rateDiscountPolicy; // 조회 대상 빈이 2개 이상일 때, 필드 명을 빈 이름으로 변경하는 방법
+    */
+
+
+    // 생성자 의존관계 주입 방법
+    // 생성자가 1개이기 때문에, @Autowired 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
 
     /* 수정자 의존관계 주입 방법
     @Autowired
@@ -29,12 +47,6 @@ public class OrderServiceImpl implements OrderService{
     @Autowired private MemberRepository memberRepository;
     @Autowired private DiscountPolicy discountPolicy;
     */
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
